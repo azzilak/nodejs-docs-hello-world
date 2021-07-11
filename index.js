1213589cc10b27e89cc48792ed2fa66bcab96ef7
azzilak <azzilakaren@students.amikom.ac.id>
2021-07-11 09:58:25 +0700

    UAS

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("20.11.3312");
+    response.end("AZZILA ISME KAREN");
 });
 
 const port = process.env.PORT || 1337;
